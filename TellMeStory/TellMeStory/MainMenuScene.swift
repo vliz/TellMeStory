e9e272d24b267922348be4f3d05245e35c3e8f32
Agnes Felicia <nicoleagnesfelicia@gmail.com>
2020-06-16 14:32:38 +0700

    added song

--- TellMeStory/TellMeStory/MainMenuScene.swift
+++ TellMeStory/TellMeStory/MainMenuScene.swift
@@ -14,7 +14,7 @@ class MainMenuScene: SKScene {
     let startButton = SKSpriteNode(imageNamed: "RightButton")
     let parentGuideButton = SKSpriteNode(imageNamed: "parentGuideButton")
     let closeButton = SKSpriteNode(imageNamed: "closeButton")
-    let backgroundSoundNode = SKAudioNode(fileNamed: "jungle.mp3")
+    let backgroundSoundNode = SKAudioNode(fileNamed: "jingle1.mp3")
     let popUpBackgroundNode = SKSpriteNode(imageNamed: "popUpBackground")
     let titleParentGuideLabel =  SKLabelNode()
     let contentParentGuideLabel =  SKLabelNode()
